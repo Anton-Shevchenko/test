2a956abc4f133bb835ac5ffe79df90d90dd23232
skill-anton <antonshevchienko@gmail.com>
2019-10-11 15:17:18 +0300

    version 1.1.2

--- src/reactify/settings.py
+++ src/reactify/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,11 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'social_django',
     'corsheaders',
     'rest_framework',
 
-    'posts',
+    'skill',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware'
 ]
 
 ROOT_URLCONF = 'reactify.urls'
@@ -68,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -75,17 +76,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'reactify.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'main',
+        'USER': 'skill',
+        'PASSWORD': 'skill',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'skill.backends.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -105,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -119,16 +128,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'), 
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-cdn-local')
-
 
 CORS_URLS_REGEX = r'^/api.*'
 CORS_ORIGIN_ALLOW_ALL = True
@@ -144,4 +151,26 @@ REST_FRAMEWORK = {
     )
 }
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = 'logout'
+LOGOUT_REDIRECT_URL = 'login'
 
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_FACEBOOK_KEY = 1750382241941952
+SOCIAL_AUTH_FACEBOOK_SECRET = '240d6974d5865e2882ad63b58821d257'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY  = '734517807385-vlcp0dqr4ohhknok4c0ujsvs27f0hutl.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '3aMuFDfuVJqj1ee9raKzzH6k'
+
+SOCIAL_AUTH_POSTGRES_JSONFIELD = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'anton.shevchenko@skill.im'
+EMAIL_HOST_PASSWORD = 'stoP78Stop'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# fix for prod
+SESSION_COOKIE_SECURE = False
--- src/skill/Controllers/Auth/auth.py
+++ src/skill/Controllers/Auth/auth.py
@@ -1,35 +1,97 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import FormView
-from django.contrib.auth import logout
+from django.http import HttpResponse
+from django.utils.encoding import force_bytes, force_text
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from ...tokens import account_activation_token
 from ...forms.auth.forms import AuthForm
-from django.contrib.auth import authenticate
+from ...forms.auth.forms import NewPassForm
+from django.contrib.auth import login, authenticate, logout
+from ...forms.auth.forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import EmailMessage
 
 
-class MyRegisterFormView(FormView):
-    form_class = UserCreationForm
-    success_url = "/"
-    template_name = "auth/registration.html"
+def activate(request, uidb64, token):
+    uid = force_text(urlsafe_base64_decode(uidb64))
+    if request.method == 'POST':
+        if request.POST.get('password1') == request.POST.get('password2'):
+            print(request.POST)
+            user = User.objects.get(pk=uid)
+            user.set_password(request.POST.get('password1'))
+            user.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('/')
+        else:
+            print('ERROR')
+    else:
+        try:
+            user = User.objects.get(pk=uid)
 
-    def form_valid(self, form):
-        form.save()
-        return super(MyRegisterFormView, self).form_valid(form)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            form = NewPassForm(request.POST)
+            return render(request, 'auth/newpass.html', {'form': form})
+        else:
+            return HttpResponse('Activation link is invalid!')
 
-    def form_invalid(self, form):
-        return super(MyRegisterFormView, self).form_invalid(form)
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            authenticate(request, email=user)
+            return redirect('/')
+    else:
+        form = SignupForm()
+    return render(request, 'auth/signup.html', {'form': form})
 
 
-def logout_view(request):
+def signin(request):
+    if request.method == 'POST':
+        try:
+            user = User.objects.filter(email=request.POST.get('email'))[0]
+            login(request, user, backend='skill.backends.EmailAuthBackend')
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist, IndexError):
+            user = None
+
+        return redirect('/signin/')
+
+    else:
+        form = AuthForm()
+        return render(request, 'auth/signin.html', {'form': form})
+
+
+def exit(request):
     logout(request)
-    return redirect('/')
+    return redirect('/signin/')
 
 
-def login_view(request):
-    form = AuthForm
-    return render(request, 'auth/login.html', {'form': form})
+def forgot(request):
+    if request.method == 'POST':
+        user = User.objects.filter(email=request.POST.get('email'))[0]
+        current_site = get_current_site(request)
+        message = render_to_string('mail/pass.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        mail_subject = 'Activate your blog account.'
+        to_email = request.POST.get('email')
+        email = EmailMessage(mail_subject, message, to=[to_email])
+        email.send()
+        return render(request, 'auth/forgot.html', {})
+    else:
+        return render(request, 'auth/forgot.html', {})
 
 
-def enter(request):
-    authenticate(email=request.POST.get('email'), password=request.POST.get('password'))
-    return redirect('/')
+def api(request):
+    return render(request, 'react.html', {})
